e0979ec3fba9bc2d517b7afdc193c83a0275217b
Luiz Filipe <99333746+Filipee01@users.noreply.github.com>
2022-07-10 12:26:37 -0300

    finished

--- js/script.js
+++ js/script.js
@@ -15,7 +15,6 @@ const loop = setInterval(()=> {
     const pipePosition = pipe.offsetLeft
     const marioPosition = +window.getComputedStyle(mario).bottom.replace('px', '')
 
-    console.log(marioPosition)
     
     if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
         pipe.style.animation = 'none'
@@ -27,7 +26,6 @@ const loop = setInterval(()=> {
         mario.src = 'images/game-over.png'
         mario.style.width = '75px'
         mario.style.marginLeft = '50px'
-        alert('Mais sorte da próxima kkkk')
         clearInterval(loop)
     }
 
